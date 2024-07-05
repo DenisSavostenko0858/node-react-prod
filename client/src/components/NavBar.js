4f201e625b226486e2c104e4fa89ceb5bc810b09
DenisSavostenko0858 <denyotube0858@gmail.com>
2024-07-06 00:30:15 +0300

    add pages for client

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -4,11 +4,14 @@ import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import Button from 'react-bootstrap/Button';
-import { NavLink } from "react-router-dom";
+import { NavLink , useNavigate} from "react-router-dom";
 import {observer} from "mobx-react-lite"
+import { ADMIN_ROUTE, LOGIN_ROUTE } from "../utils/consts";
 
 const NavBar = observer(() => {
     const {user} = useContext(Context);
+    const navigate = useNavigate();
+
     return (
     <Navbar expand="lg" className="bg-body-tertiary">
       <Container fluid>
@@ -17,12 +20,12 @@ const NavBar = observer(() => {
         <Navbar.Collapse id="navbarScroll">
             {user.isAuth ?
                 <Nav className="ml-auto" navbarScroll style={{marginLeft:"50px"}}>
-                    <Button>Управление</Button>
-                    <Button style={{marginLeft:"50px"}}>Войти</Button>
+                    <Button onClick={()=> navigate(ADMIN_ROUTE)}>Управление</Button>
+                    <Button style={{marginLeft:"50px"}}>Выйти</Button>
                 </Nav>
             :
                 <Nav className="ml-auto" navbarScroll style={{marginLeft:"50px"}}>
-                    <Button onClick={()=> user.setIsAuth(true)}>Авторизация</Button>
+                    <Button onClick={()=> navigate(LOGIN_ROUTE)}>Авторизация</Button>
                 </Nav>
             }
         </Navbar.Collapse>
--- client/src/page/AdminPanel.js
+++ client/src/page/AdminPanel.js
@@ -1,11 +1,42 @@
-import React from 'react';
+import React, {useState} from 'react';
+import {Button, Container} from "react-bootstrap";
+import CreateBrand from "../components/modals/CreateBrand";
+import CreateProduct from "../components/modals/CreateProduct";
+import CreateType from "../components/modals/CreateType";
 
-const AdminPanel = () => {
-  return (
-    <div>
-        AdminPanel
-    </div>
-  );
-}
+const Admin = () => {
+    const [brandVisible, setBrandVisible] = useState(false)
+    const [typeVisible, setTypeVisible] = useState(false)
+    const [deviceVisible, setDeviceVisible] = useState(false)
 
-export default AdminPanel;
+    return (
+        <Container className="d-flex flex-column">
+            <Button
+                variant={"outline-dark"}
+                className="mt-4 p-2"
+                onClick={() => setTypeVisible(true)}
+            >
+                Добавить тип
+            </Button>
+            <Button
+                variant={"outline-dark"}
+                className="mt-4 p-2"
+                onClick={() => setBrandVisible(true)}
+            >
+                Добавить бренд
+            </Button>
+            <Button
+                variant={"outline-dark"}
+                className="mt-4 p-2"
+                onClick={() => setDeviceVisible(true)}
+            >
+                Добавить устройство
+            </Button>
+            <CreateBrand show={brandVisible} onHide={() => setBrandVisible(false)}/>
+            <CreateProduct show={deviceVisible} onHide={() => setDeviceVisible(false)}/>
+            <CreateType show={typeVisible} onHide={() => setTypeVisible(false)}/>
+        </Container>
+    );
+};
+
+export default Admin;
